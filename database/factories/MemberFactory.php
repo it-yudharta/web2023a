4a39b1056628a02c67744aedfcc6365948ba86c6
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2023-02-27 14:34:29 +0700

    data member

---
+++ database/factories/MemberFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Member>
+ */
+class MemberFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name,
+            'address' => fake()->address,
+            'age' => fake()->numberBetween(15, 60),
+            'phone' => fake()->phoneNumber,
+        ];
+    }
+}

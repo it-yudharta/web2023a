daa6a6f767c06b68aede9670ab68b27e85288711
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2023-02-27 14:10:17 +0700

    home routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/', function() {
+    return view('home');
+});
+
+
+
+
+// dinamic routing
+Route::get('/halo/{nama?}', function($nama = 'Dunia') {
+    return 'Halo ' . $nama;
 });
